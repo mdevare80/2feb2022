b4f64dc658a9d024ab87d14f36b70cef8adc275d
shwetaeladi07 <74300968+shwetaeladi07@users.noreply.github.com>
2022-02-09 20:07:27 +0530

    Code

---
+++ code.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UFT-8">
+    <meta http-eqiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, inital-scale=1.0">
+    <title>Form Handle</title>
+</head>
+</body>
+<form action="index.php" method="post">
+    Display my Name :
+    <input type="text" name = "fname">
+    <br>
+    <input type="submit">
+</form>
+</body>
+</html>
