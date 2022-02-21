6adce163015def221fd39fb50ba6972349911087
aman693 <65719277+aman693@users.noreply.github.com>
2022-02-21 19:09:24 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -4,5 +4,6 @@ public static void main(String args[])
 {
 System.out.println("This program is written for Git and GitHub Demo");
 System.out.println(" second line");
+ System.out.println("connecting jenkins to github");
 }
 }
