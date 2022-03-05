d296a19dd8915e12a08bafc92f53eba7b3b75726
Aman <amanvinod945@gmail.com>
2022-03-05 17:06:08 +0530

    for Devops lab on jenkins

---
+++ array.java
@@ -0,0 +1,18 @@
+public class array {
+   public static void main(String args[]){
+      int temp, size;
+      int array[] = {10, 20, 25, 63, 96, 57, 33, 92, 82, 94};
+      size = array.length;
+
+      for(int i = 0; i<size; i++ ){
+         for(int j = i+1; j<size; j++){
+            if(array[i]>array[j]){
+               temp = array[i];
+               array[i] = array[j];
+               array[j] = temp;
+            }
+         }
+      }
+      System.out.println("Third largest element is:: "+array[size-1]);
+   }
+}
